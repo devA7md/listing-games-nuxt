f0dd21e1f9c4dea01427dafd5b07e2f6ac35838d
Ahmed Atia <dev.a7md@outlook.com>
2021-04-02 03:44:43 +0200

    initial setup

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="m-5">
+    <div class="text-gray-500 text-5xl">Games</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style></style>
